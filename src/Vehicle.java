9e1341439cfa8400a1557bfd10b58fcf8e2742bf
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-28 17:54:03 +0200

    Added method: range

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,4 +2,9 @@ public class Vehicle {
     int passengers; // number of passengers
     int fuelCap; // fuel capacity in gallons
     int mpg; // fuel consumption in miles per gallon
+
+    // Method for displaying trip distance
+    void range(){
+        System.out.println("Range in miles on a full tank: " + fuelCap * mpg);
+    }
 }
--- src/VehicleDemo.java
+++ src/VehicleDemo.java
@@ -14,15 +14,14 @@ public class VehicleDemo {
         sportCar.fuelCap = 14;
         sportCar.mpg = 12;
 
-        //Calculating the travel distance with a full tank
-        //minivan
-        range1 = minivan.fuelCap * minivan.mpg;
-        // sport-car
-        range2 = sportCar.fuelCap * sportCar.mpg;
         System.out.println("The minivan can carry " + minivan.passengers +
-                " passengers for a distance of " + range1 + " miles");
+                " passengers");
+
+        minivan.range(); // Displays the range of minivan on a full tank
 
         System.out.println("The sport-car can carry " + sportCar.passengers +
-                " passengers for a distance of " + range2 + " miles");
+                " passengers");
+
+        sportCar.range(); // Displays the range of sport-car on a full tank
     }
 }
