01c8bd34df366b3ff9f41e0b62dfd8adc3d9aa93
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-29 14:16:54 +0200

    Added a method with parameters for calculating the amount of fuel required to travel a given distance.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,8 +3,21 @@ public class Vehicle {
     int fuelCap; // fuel capacity in gallons
     int mpg; // fuel consumption in miles per gallon
 
+    /*
     // Method for displaying trip distance
-    int range(){
-       return fuelCap * mpg;
+    int range() {
+        return fuelCap * mpg;
     }
+
+     */
+
+    /*
+    The method for calculating the amount of fuel
+    required to a given distance
+     */
+    double fuelNeeded(int miles) {
+        return (double) miles / mpg;
+    }
+
+
 }
--- src/VehicleDemo.java
+++ src/VehicleDemo.java
@@ -2,27 +2,32 @@ public class VehicleDemo {
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
         Vehicle sportCar = new Vehicle();
-        int range1, range2;
+        double gallons;
+        int distance = 252;
 
         //Assigning values to fields in minivan
         minivan.passengers = 7;
         minivan.fuelCap = 16;
         minivan.mpg = 21;
 
-        range1 = minivan.range(); // Calculating the range of minivan on a full tank
-
         //Assigning values to fields in sport-car
         sportCar.passengers = 2;
         sportCar.fuelCap = 14;
         sportCar.mpg = 12;
 
-        range2 = sportCar.range(); // Calculating the range of sport-car on a full tank
+        /*
+        Calculating of the volume of fuel required for
+        a minivan to travel a given distance
+         */
+        gallons = minivan.fuelNeeded(distance);
+        System.out.println("The minivan needs " + gallons + " gallons of fuel to" +
+                " travel " + distance + " miles");
 
-        System.out.println("The minivan can carry " + minivan.passengers +
-                " passengers for " + range1 + " miles on a full tank");
+        gallons = sportCar.fuelNeeded(distance);
+        System.out.println("The sport-car needs " + gallons + " gallons of fuel to" +
+                " travel " + distance + " miles");
 
-        System.out.println("The sport-car can carry " + sportCar.passengers +
-                " passengers for " + range2 + " miles on a full tank");
+
 
 
     }
